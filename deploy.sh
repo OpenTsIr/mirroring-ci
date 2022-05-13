2d082f427c0a450eddf0de080c9d14ead4ec86c8
BaHmAnBs <bahmanbs@hotmail.com>
2022-05-13 17:00:11 +0430

    fix: checkout before running deploy command

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,3 @@
 PROJECT_DIRECTORY="/home/user1/mirroring"
 
-git --work-tree=$PROJECT_DIRECTORY --git-dir=$PROJECT_DIRECTORY/.git checkout main $PROJECT_DIRECTORY
-
 docker compose -f $PROJECT_DIRECTORY/docker-compose.yaml up --build
